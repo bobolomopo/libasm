7824b62ca16a72f6fc62881557fc447c7df0707f
Julien Andre <jandre@fu-r6-p5.s19.be>
2021-02-15 16:00:10 +0100

    ok

--- ft_strlen.s
+++ ft_strlen.s
@@ -4,15 +4,16 @@
     %define M_FT_STRLEN _ft_strlen
 %endif
 
+; prototype : size_t strlen(const char *s);
 ; defining global name depending OS
 
 segment .text
     global M_FT_STRLEN
 
 M_FT_STRLEN:
-    mov eax, -1                     ; debute le compte a -1 vu qu'on commence par un increment
+    MOV eax, -1                     ; debute le compte a -1 vu qu'on commence par un increment
 FT_STRLEN_LOOP:
-    inc eax                         ; incremente le compte
-    cmp BYTE [rdi + rax], 0         ; compare si on est arrive a '\0'
-    jne FT_STRLEN_LOOP              ; si la comparaison n'est pas egale on relance la boucle
-    ret                             ; fin de procedure
+    INC eax                         ; incremente le compte
+    CMP BYTE [rdi + rax], 0         ; compare si on est arrive a '\0'
+    JNE FT_STRLEN_LOOP              ; si la comparaison n'est pas egale on relance la boucle
+    RET                             ; fin de procedure
